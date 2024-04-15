9d55cdc1ba717acb5829b68ca78ec8322913ba3f
BLACK WOLF <ageo.carhuaricra.27@unsch.edu.pe>
2024-04-15 10:27:03 -0500

    Estamos implementando un datagrid en el componente home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,47 @@
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid"
+import { People } from "../../data/people";
 
-export const Home = () => {
+export interface HomeInterface{
+
+}
+/**Instalamos el datagrid para hacer uso en este componente de npm install @mui/x-data-grid */
+
+export const Home:React.FC<HomeInterface> = () => {
+
+    const pageSize = 5;
+    //columnas que va utilizar el datagrid
+    const columns = [
+        {
+            field:'name',
+            headerName:'Name',
+            flex:1,
+            minWidth:150,
+            renderCell:(params: GridRenderCellParams) => <>{params.value}</>//+ params.row.name
+        },
+        {
+            field:'category',
+            headerName:'Categories',
+            flex:1,
+            renderCell:(params: GridRenderCellParams) => <>{params.value}</>//+ params.row.name
+        },
+        {
+            field:'company',
+            headerName:'Companies',
+            flex:1,
+            renderCell:(params: GridRenderCellParams) => <>{params.value}</>//+ params.row.name
+        }
+    ]
+
     return (
-        <div>Home</div>
+        <DataGrid
+                rows={People}
+                columns={columns}
+                disableColumnSelector
+                disableRowSelectionOnClick
+                autoHeight
+                pagination
+                getRowId={(row:any )=> row.id}>
+                
+        </DataGrid>
     )
 }
